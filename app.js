ea6e33a12dfaebdcd357b799adfbdb18725acee0
Aaditya Raj Kumawat <aaditya01work@gmail.com>
2020-07-09 22:50:27 +0530

    added extra featurers

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ const newMeeting = createMeeting('Increasing Sales', 'Marketing Department');
 
 if (age > 18) {
     console.log('Agenada:', newMeeting[0]);
+} else if (age < 18) {
+    console.log('Sorry you are not invited for the meeting');
 }
+
+const childAnnualMeet = (festival) => {
+    console.log(festival);
+};
